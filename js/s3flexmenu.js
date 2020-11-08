3676064fb79676427a6330a2812d53a832fdc95e
francyfox <7info7web@gmail.com>
2020-11-08 23:33:55 +0600

    Flexmenu is work

--- js/s3flexmenu.js
+++ js/s3flexmenu.js
@@ -6,22 +6,42 @@ document.addEventListener("DOMContentLoaded", function(event) {
         menu_width = menu.scrollWidth,
         tag = menu.querySelectorAll('a'),
         menu_folder = menu.querySelectorAll('li'),
+        MaxWidth = 0,
+        TagsWidth = [];
 
         more = document.createElement('li');
 
         more.classList.add('more-button');
-        more.innerHTML = '<ul class="more-dropdawn"></ul>'
+        more.innerHTML = '<a href="#">...</a><ul class="more-dropdawn"></ul>';
 
-    for (var i = 0; i < menu_folder.length; i++)
+
+    function RemoveOpened (folder)
     {
+        folder.classList.remove('_opened');
+    }
+
+    if (menuOffsetWidth < menu_width)
+    {
+        menu.querySelector('ul').append(more);
+    }
+
+    for (var i = 0, count = 0; i < menu_folder.length; i++)
+    {
+
+        if (menu_folder[i].parentNode.parentNode.classList.contains('menu')){
+            TagsWidth[count] = menu_folder[i].offsetWidth;
+
+            if (MaxWidth < menuOffsetWidth - more.offsetWidth){
+                MaxWidth += TagsWidth[count];
+            }else{
+                more.querySelector('ul.more-dropdawn').append(menu_folder[i]);
+            }
+            count++;
+        }
+
         menu_folder[i].addEventListener('mouseenter', function (e) {
             this.classList.add('_opened');
         });
-
-        function RemoveOpened (folder)
-        {
-            folder.classList.remove('_opened');
-        }
 
         menu_folder[i].addEventListener('mouseleave', function ()
         {
@@ -30,18 +50,9 @@ document.addEventListener("DOMContentLoaded", function(event) {
              TODO: Stop timeout if mousenter
             */
         });
-
-
-
     }
 
-    function AddMore ()
-    {
-        if (menuOffsetWidth < menu_width)
-        {
-            console.log('flexmenu activated');
-        }
-    }
+    var last = menu.children[0].childElementCount - 2;
+    more.querySelector('ul.more-dropdawn').prepend(menu.children[0].children[last]);
 
-    AddMore();
 });
