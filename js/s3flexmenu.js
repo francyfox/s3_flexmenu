52f455e85420aa13cd7b95683baabfbd711f0d04
francyfox <7info7web@gmail.com>
2020-11-10 10:47:06 +0600

    refactor initFlex, get React

--- js/s3flexmenu.js
+++ js/s3flexmenu.js
@@ -4,55 +4,81 @@ document.addEventListener("DOMContentLoaded", function(event) {
         menu = document.querySelector('.menu'),
         menuOffsetWidth = menu.offsetWidth,
         menu_width = menu.scrollWidth,
-        tag = menu.querySelectorAll('a'),
         menu_folder = menu.querySelectorAll('li'),
+        submenu = menu.children[0].querySelectorAll('ul'),
         MaxWidth = 0,
         TagsWidth = [];
 
-        more = document.createElement('li');
+    function options (more_text, MoreClass, MoreDrapdawnClass)
+    {
+        this.more_text = (more_text) ? more_text: '...',
+            this.MoreClass = (MoreClass) ? MoreClass: 'more-button',
+            this.MoreDrapdawnClass = (MoreDrapdawnClass) ? MoreDrapdawnClass: 'more-dropdawn';
 
-        more.classList.add('more-button');
-        more.innerHTML = '<a href="#">...</a><ul class="more-dropdawn"></ul>';
-
+        return this;
+    }
 
     function RemoveOpened (folder)
     {
         folder.classList.remove('_opened');
     }
 
-    if (menuOffsetWidth < menu_width)
+    function initFlexMenu ( options )
     {
-        menu.querySelector('ul').append(more);
-    }
+        if (menuOffsetWidth < menu_width)
+        {
+            more = document.createElement('li');
 
-    for (var i = 0, count = 0; i < menu_folder.length; i++)
-    {
+            more.classList.add(options.MoreClass);
+            more.innerHTML = '<a href="#">'+options.more_text+'</a><ul class=" '+options.MoreDrapdawnClass+' "></ul>';
 
-        if (menu_folder[i].parentNode.parentNode.classList.contains('menu')){
-            TagsWidth[count] = menu_folder[i].offsetWidth;
-
-            if (MaxWidth < menuOffsetWidth - more.offsetWidth){
-                MaxWidth += TagsWidth[count];
-            }else{
-                more.querySelector('ul.more-dropdawn').append(menu_folder[i]);
-            }
-            count++;
+            menu.querySelector('ul').append(more);
         }
 
-        menu_folder[i].addEventListener('mouseenter', function (e) {
-            this.classList.add('_opened');
-        });
-
-        menu_folder[i].addEventListener('mouseleave', function ()
+        for (var i = 0, count = 0; i < menu_folder.length; i++)
         {
-            var folder = this;
-            setTimeout(function () { RemoveOpened(folder) }, 800); /*
-             TODO: Stop timeout if mousenter
-            */
+
+            if (menu_folder[i].parentNode.parentNode.classList.contains('menu')){
+                TagsWidth[count] = menu_folder[i].offsetWidth;
+
+                if (MaxWidth < menuOffsetWidth - more.offsetWidth){
+                    MaxWidth += TagsWidth[count];
+                }else{
+                    more.querySelector("ul."+options.MoreDrapdawnClass).append(menu_folder[i]);
+                }
+                count++;
+            }
+
+            menu_folder[i].addEventListener('mouseover', function (e) {
+                this.removeEventListener('mouseleave', function (){});
+                this.classList.add('_opened');
+            });
+
+            menu_folder[i].addEventListener('mouseleave', function ()
+            {
+                var folder = this;
+                setTimeout(function () {
+                    RemoveOpened(folder)
+                }, 800);
+            });
+        }
+
+        var last = menu.children[0].childElementCount - 2;
+        more.querySelector('ul.'+options.MoreDrapdawnClass).prepend(menu.children[0].children[last]);
+    }
+
+    function getReact ()
+    {
+        submenu.forEach(function(item, i, arr){
+            console.log(item.parentElement);
+            return console.log(item.parentElement.getBoundingClientRect());
+            //.getBoundingClientRect()
         });
     }
 
-    var last = menu.children[0].childElementCount - 2;
-    more.querySelector('ul.more-dropdawn').prepend(menu.children[0].children[last]);
+    getReact();
+    const opt = new options();
+    initFlexMenu(opt);
+
 
 });
